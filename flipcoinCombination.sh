095cd164abccd312c8738d1f0c92343b8d307454
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-10-11 03:45:39 +0530

    double coins combination

--- flipcoinCombination.sh
+++ flipcoinCombination.sh
@@ -1,16 +1,32 @@
-heads=0;
-tails=0;
+#!/bin/bash
 read -p "Enter number of flip required " n
 for (( i=0; i<n; i++ ))
 do
-	a=$(( RANDOM%2 ));
-	if [ $a == 1 ];
+	s=$(( RANDOM%2 ));
+	if [ $s == 1 ];
 	then
          	heads=$(($heads+1))
 	else
         	tails=$(($tails+1))
 	fi
+	d=$(( RANDOM%4 ));
+	case $d in
+		0) hh=$(($hh+1))
+			;;
+		1) ht=$(($ht+1))
+			;;
+		2) th=$(($th+1))
+			;;
+		3) tt=$(($tt+1))
+			;;
+		*) echo "something went  wrong"
+			;;
+	esac
 done
 
-echo "Number of head win" $heads
-echo "Number of tail win" $tails
+echo "Number of heads " $heads
+echo "Number of tails " $tails
+echo "Number of hh combination " $hh
+echo "Number of ht combination " $ht
+echo "Number of th combination " $th
+echo "Number of tt combination " $tt
