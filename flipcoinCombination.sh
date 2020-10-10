e154767b94890ef109b7b398cac59b79b9541228
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-10-11 04:00:16 +0530

    triple coins combination

--- flipcoinCombination.sh
+++ flipcoinCombination.sh
@@ -22,6 +22,27 @@ do
 		*) echo "something went  wrong"
 			;;
 	esac
+	t=$(( RANDOM%7 ));
+        case $t in
+                0) hhh=$(($hhh+1))
+                        ;;
+                1) hht=$(($hht+1))
+                        ;;
+                2) hth=$(($hth+1))
+                        ;;
+                3) htt=$(($htt+1))
+                        ;;
+		4) thh=$(($hth+1))
+                        ;;
+                5) tht=$(($tht+1))
+                        ;;
+                6) tth=$(($tth+1))
+                        ;;
+                7) ttt=$(($ttt+1))
+                        ;;
+		*) echo "something went  wrong"
+                        ;;
+	esac
 done
 
 echo "Number of heads " $heads
@@ -30,3 +51,11 @@ echo "Number of hh combination " $hh
 echo "Number of ht combination " $ht
 echo "Number of th combination " $th
 echo "Number of tt combination " $tt
+echo "Number of hhh combination " $hhh
+echo "Number of hht combination " $hht
+echo "Number of hth combination " $hth
+echo "Number of htt combination " $htt
+echo "Number of thh combination " $thh
+echo "Number of tht combination " $tht
+echo "Number of tth combination " $tth
+echo "Number of ttt combination " $ttt
