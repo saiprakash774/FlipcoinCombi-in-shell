a330cfc46f03b7857734d92e66aa6ae0933ef683
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-10-11 03:04:03 +0530

    single coin multiple flips

--- flipcoinCombination.sh
+++ flipcoinCombination.sh
@@ -1,8 +1,16 @@
-#!/bin/bash
-a=$(( RANDOM%2 ));
-if [ $a == 1 ];
-then
-         echo "heads"
-else
-        echo "tails"
-fi
+heads=0;
+tails=0;
+read -p "Enter number of flip required " n
+for (( i=0; i<n; i++ ))
+do
+	a=$(( RANDOM%2 ));
+	if [ $a == 1 ];
+	then
+         	heads=$(($heads+1))
+	else
+        	tails=$(($tails+1))
+	fi
+done
+
+echo "Number of head win" $heads
+echo "Number of tail win" $tails
